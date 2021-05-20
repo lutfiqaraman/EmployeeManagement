ededf203b5212c5707e946210c24e3fbea271780
lutfiqaraman <lutfiqaraman@gmail.com>
2021-05-20 21:06:37 +0300

    Create MVC project

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace EmployeeManagement
 {
@@ -20,6 +21,7 @@ namespace EmployeeManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,6 +33,9 @@ namespace EmployeeManagement
             }
 
             app.UseRouting();
+            app.UseStaticFiles();
+
+            app.UseMvcWithDefaultRoute();
 
             app.UseEndpoints(endpoints =>
             {
