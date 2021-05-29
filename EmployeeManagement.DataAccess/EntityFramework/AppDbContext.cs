156932669098747b6dfebb02313aecf6de252ff4
lutfiqaraman <lutfiqaraman@gmail.com>
2021-05-29 12:18:54 +0300

    Seed data using EF

--- EmployeeManagement.DataAccess/EntityFramework/AppDbContext.cs
+++ EmployeeManagement.DataAccess/EntityFramework/AppDbContext.cs
@@ -11,5 +11,16 @@ namespace EmployeeManagement.DataAccess.EntityFramework
         }
 
         public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasData
+                (
+                    new Employee() { Id = 1, Name = "Mary", Department = Department.HR, Email = "mary@mockemp.com" },
+                    new Employee() { Id = 2, Name = "Jade", Department = Department.IT, Email = "jade@mockemp.com" },
+                    new Employee() { Id = 3, Name = "John", Department = Department.IT, Email = "john@mockemp.com" },
+                    new Employee() { Id = 4, Name = "Ryan", Department = Department.Financial, Email = "ryan@mockemp.com" }
+                );
+        }
     }
 }
---
+++ EmployeeManagement.DataAccess/Migrations/20210529090631_SeedEmployeeData.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeManagement.DataAccess.Migrations
+{
+    public partial class SeedEmployeeData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Department", "Email", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "mary@mockemp.com", "Mary" },
+                    { 2, 2, "jade@mockemp.com", "Jade" },
+                    { 3, 2, "john@mockemp.com", "John" },
+                    { 4, 3, "ryan@mockemp.com", "Ryan" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
