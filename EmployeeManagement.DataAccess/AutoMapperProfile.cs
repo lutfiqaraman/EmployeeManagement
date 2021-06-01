84350c95d16f75f87915f6cb3d90572be4b9dd6b
lutfiqaraman <lutfiqaraman@gmail.com>
2021-06-01 09:44:24 +0300

    remove unsed code

--- EmployeeManagement.DataAccess/AutoMapperProfile.cs
+++ EmployeeManagement.DataAccess/AutoMapperProfile.cs
@@ -1,11 +1,6 @@
 ﻿using AutoMapper;
 using EmployeeManagement.DataAccess.Repositories.Employees.Dto;
 using EmployeeManagement.Domain.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EmployeeManagement.DataAccess
 {
