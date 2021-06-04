6b9750206f372bf0f55762dbd50a62b9f9559961
lutfiqaraman <lutfiqaraman@gmail.com>
2021-06-04 10:21:56 +0300

    create a viewmodel to create an employee

---
+++ EmployeeManagement/Models/Employees/EmployeeCreateViewModel.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeManagement.Domain.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace EmployeeManagement.Presentation.Models.Employees
+{
+    public class EmployeeCreateViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public Department Department { get; set; }
+        public IFormFile Photo { get; set; }
+    }
+}
