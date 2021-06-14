5da4e88d32d0f1b2e5f95671a54dee4e303d8812
lutfiqaraman <lutfiqaraman@gmail.com>
2021-06-14 21:14:43 +0300

    Create AccountController

---
+++ EmployeeManagement/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Presentation.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
