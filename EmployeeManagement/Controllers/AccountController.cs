e7001133073e8886563f82c17b248cde722b4919
lutfiqaraman <lutfiqaraman@gmail.com>
2021-06-16 20:17:17 +0300

    create a user

--- EmployeeManagement/Controllers/AccountController.cs
+++ EmployeeManagement/Controllers/AccountController.cs
@@ -1,14 +1,21 @@
 ﻿using EmployeeManagement.Presentation.Models.Users;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace EmployeeManagement.Presentation.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<IdentityUser>   UserManager;
+        private readonly SignInManager<IdentityUser> SignInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            UserManager   = userManager;
+            SignInManager = signInManager;
+        }
+
         [HttpGet]
         public IActionResult Register()
         {
@@ -16,8 +23,25 @@ namespace EmployeeManagement.Presentation.Controllers
         }
 
         [HttpPost]
-        public IActionResult Register(RegisterViewModel model)
+        public async Task<IActionResult> Register(RegisterViewModel model)
         {
+            if (ModelState.IsValid)
+            {
+                IdentityUser user = new IdentityUser { UserName = model.Email, Email = model.Email };
+                IdentityResult result = await UserManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await SignInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
             return View(model);
         }
     }
