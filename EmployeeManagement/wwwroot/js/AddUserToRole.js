b6d515b49fbefa08224df6915f83633eb4fa10b9
lutfiqaraman <lutfiqaraman@gmail.com>
2021-07-10 00:21:54 +0300

    add or remove a user to a role

--- EmployeeManagement/wwwroot/js/AddUserToRole.js
+++ EmployeeManagement/wwwroot/js/AddUserToRole.js
@@ -1,9 +1,1 @@
-﻿$(document).ready(function () {
-    $("#addUser").click(function () {
-        $("#addUserModal").modal('show');
-    });
-
-    //$("#btnHideModal").click(function () {
-    //    $("#loginModal").modal('hide');
-    //});
-});
+﻿
