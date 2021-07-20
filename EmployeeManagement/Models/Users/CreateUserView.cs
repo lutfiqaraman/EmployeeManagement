6dbf09882150561a1d3f611fe78df970f4d70e3d
lutfiqaraman <lutfiqaraman@gmail.com>
2021-07-20 23:08:45 +0300

    Create a user process

---
+++ EmployeeManagement/Models/Users/CreateUserView.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.Presentation.Models.Users
+{
+    public class CreateUserView
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
