0802ed234d33d33fbb1840c0a2e394d08aff79d4
lutfiqaraman <lutfiqaraman@gmail.com>
2021-08-09 23:38:16 +0300

    Create a UserClaimsViewModel

---
+++ EmployeeManagement/Models/Claims/UserClaimsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace EmployeeManagement.Presentation.Models.Claims
+{
+    public class UserClaimsViewModel
+    {
+        public UserClaimsViewModel()
+        {
+            List<UserClaim> Claims = new List<UserClaim>();
+        }
+
+        public string UserId { get; set; }
+        public List<UserClaim> Claims { get; set; }
+    }
+}
