a7157fc167ea846d8c0aa48bb805bdf6596f72ed
lutfiqaraman <lutfiqaraman@gmail.com>
2021-10-12 05:48:30 +0300

    Create superadmin handler

---
+++ EmployeeManagement/Authorization/SuperAdminHandler.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Presentation.Authorization
+{
+    public class SuperAdminHandler : AuthorizationHandler<ManageAdminRolesAndClaimsRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ManageAdminRolesAndClaimsRequirement requirement)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
